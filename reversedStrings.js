53ac2556e66093ecb7d6bb0ff007dada161a3e8f
Dave <dave.w.mckinnon@gmail.com>
2022-10-17 17:40:12 -0700

    add reversed strings problem

---
+++ reversedStrings.js
@@ -0,0 +1,9 @@
+const solution = str => str.split('').reverse().join('');
+
+// const solution = (str) => {
+//   let result = ""
+//   for (let i = str.length-1; i >= 0; i--) {
+//     result += str[i]
+//   }
+//   return result;
+// }
