7cafacb8000ecf47b865dd10f314878aaa79d23a
Dave <dave.w.mckinnon@gmail.com>
2022-11-14 16:36:55 -0800

    refactor DNAStrand functin

--- complimentaryDNA.js
+++ complimentaryDNA.js
@@ -1,20 +1,20 @@
-const DNAStrand = (dna) => {
-  let newDNA = "";
-  for (let i = 0; i < dna.length; i++) {
-    if (dna[i] === "A") {
-      newDNA += "T";
-    }
-    if (dna[i] === "T") {
-      newDNA += "A";
-    }
-    if (dna[i] === "G") {
-      newDNA += "C";
-    }
-    if (dna[i] === "C") {
-      newDNA += "G";
-    }
-  }
-  return newDNA;
-};
+// const DNAStrand = (dna) => {
+//   let newDNA = "";
+//   for (let i = 0; i < dna.length; i++) {
+//     if (dna[i] === "A") {
+//       newDNA += "T";
+//     }
+//     if (dna[i] === "T") {
+//       newDNA += "A";
+//     }
+//     if (dna[i] === "G") {
+//       newDNA += "C";
+//     }
+//     if (dna[i] === "C") {
+//       newDNA += "G";
+//     }
+//   }
+//   return newDNA;
+// };
 
-console.log(DNAStrand("ATTGC"));
+const DNAStrand = dna => dna.replace(/A/g, 't').replace(/T/g, 'a').replace(/C/g, 'g').replace(/G/g, 'c').toUpperCase();
